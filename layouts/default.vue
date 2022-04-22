fcbe0350bc77ca4d859035cb5b067e2deb4be100
singhaltd <85279370+singhaltd@users.noreply.github.com>
2022-04-22 20:41:52 +0700

    first

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup>
+    const route = useRoute();
+
+    useHead({
+    meta: [{ name: "og:title", content: `App Name - ${route.meta.title}` }],
+    });
+</script>
+
+<template>
+  <div>
+    <NavbarMega />
+    <NuxtPage />
+    <FooterTwof />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<script setup>
+definePageMeta({
+  title: "Some Page",
+});
+</script>
+<template>
+  <main>
+    <div>
+      <figure>
+        <img
+          src="https://www.kasikornbank.com/SiteCollectionDocuments/personal/img/banner/3000x1000-xpl.jpg"
+        />
+      </figure>
+    </div>
+    <div class="w-8/12 mx-auto">
+      <div class="grid grid-cols-3 grid-row-2 gap-2">
+        <template v-for="i in 5" :key="i">
+          <div :class="i == 1 ? 'row-span-2' : ''">
+            <div class="card w-full">
+              <!-- <figure> -->
+              <img
+                src="https://www.kasikornbank.com/SiteCollectionDocuments/personal/img/spotlightS/220224-lot1_npa_SpotlightS_mainpage_560x480px.jpg"
+                alt="Shoes"
+              />
+              <!-- </figure> -->
+            </div>
+          </div>
+        </template>
+      </div>
+    </div>
+  </main>
+</template>
